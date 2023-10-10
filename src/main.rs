a48131e9cc6384d701136f2939cc3d1690196ba1
sltlala <sltlala@163.com>
2023-10-10 16:47:24 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {              // 程序入口
+    let mut x: i32 = 27;  // 可变变量绑定
+    print!("{x}");       // 与 printf 类似的输出宏
+    while x != 1 {       // 表达式周围没有括号
+        if x % 2 == 0 {  // 与其他语言类似的数值计算
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    print!("\n");
+    println!("finish!!!");
 }
