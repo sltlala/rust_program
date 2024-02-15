dc42904ddb0ec5d00c22c1f4d8fe4a3f02b2948d
sltlala <sltlala@163.com>
2024-02-15 14:37:18 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-fn main() {              // 程序入口
-    let mut x: i32 = 27;  // 可变变量绑定
-    print!("{x}");       // 与 printf 类似的输出宏
-    while x != 1 {       // 表达式周围没有括号
-        if x % 2 == 0 {  // 与其他语言类似的数值计算
-            x = x / 2;
-        } else {
-            x = 3 * x + 1;
-        }
-        print!(" -> {x}");
-    }
-    print!("\n");
-    println!("finish!!!");
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{:?}", v)
 }
