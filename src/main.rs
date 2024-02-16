1115258cd2e2450aa1f5c57d46faebd246681b44
sltlala <sltlala@163.com>
2024-02-16 15:25:22 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
-fn main() {
-    for i in 0..1000000 {
-        println!("fib({i}): {}",fibonacci(i))
+use std::io;
+use std::io::Write;
+use std::io::Error;
+use std::fs::File;
+use std::time::{Instant};
+fn main() -> Result<(), Error> {
+    println!("Please input your number.");
+    let mut number = String::new();
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line.");
+    let i:u32 = number.trim().parse().expect("Please type a number!");
+    let mut times_list: Vec<String> = Vec::new();
+
+    for number in 0..i {
+        let time = collect_times(number);
+        times_list.push(time);
     }
+    let file_path = "fibonacci_result.txt";
+    let mut file = File::create(file_path)?;
+    for item in &times_list {
+        writeln!(file, "{}", item)?; // 将每个元素写入文件，并添加换行符
+    }
+    println!("{:?}",times_list);
+    Ok(())
 }
 
-fn fibonacci(n:u128) -> u128 {
+fn fibonacci(n:u32) -> u32 {
     if n <= 1 {
         return n;
     }
     return fibonacci(n - 1) + fibonacci(n -2);
+}
+
+fn collect_times(number:u32) -> String{
+    let start = Instant::now();
+    fibonacci(number);
+    //print!("fib({i}): {}",fibonacci(i));
+    let end = Instant::now();
+    let elapsed = end - start;
+    let time = format!("{:?}",elapsed.as_nanos());
+    return time
 }
